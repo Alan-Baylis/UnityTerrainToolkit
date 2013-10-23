1020018ab6be37ea13214fda4a90b9a7403edafc
Jamie <JanJansen2634@gmail.com>
2013-10-23 20:49:58 +0100

    Can reduce control texture size too now

--- TerrainTextureExporter/Assets/Editor/TextureData.cs
+++ TerrainTextureExporter/Assets/Editor/TextureData.cs
@@ -8,16 +8,48 @@ public class TextureData
 
     public void AdjustSplatMapResolution(int newResolution)
     {
-        var oldExponent = (int)Math.Log(ControlTextureResolution, 2);
+        var oldExponent = (int) Math.Log(ControlTextureResolution, 2);
 
-        var newExponent = (int)Math.Log(newResolution, 2);
+        var newExponent = (int) Math.Log(newResolution, 2);
 
         if (newExponent > oldExponent)
         {
             ExpandSplatMaps(newExponent - oldExponent);
+        } else if (oldExponent > newExponent)
+        {
+            ShrinkSplatMaps(oldExponent - newExponent);
         }
 
         ControlTextureResolution = newResolution;
+    }
+
+    private void ShrinkSplatMaps(int delta)
+    {
+        var splatmapSizeModifier = (int) Math.Pow(2, delta);
+
+        var newFirstCount = (SplatMaps.GetUpperBound(0) + 1) / splatmapSizeModifier;
+        var newSecondCount = (SplatMaps.GetUpperBound(1) + 1) / splatmapSizeModifier;
+        var thirdCount = SplatMaps.GetUpperBound(2) + 1;
+
+        var newSplatMaps = new float[newFirstCount, newSecondCount, thirdCount];
+
+        for (var x = 0; x < newFirstCount; x++)
+        {
+            for (var z = 0; z < newSecondCount; z++)
+            {
+// ReSharper disable SuggestUseVarKeywordEverywhere -- we want to be clear this is an integer division and we want a compile error if that changes. 
+                int origX = x * splatmapSizeModifier;
+                int origZ = z * splatmapSizeModifier;
+// ReSharper restore SuggestUseVarKeywordEverywhere
+
+                for (var i = 0; i < thirdCount; i++)
+                {
+                    newSplatMaps[x, z, i] = SplatMaps[origX, origZ, i];
+                }
+            }
+        }
+
+        SplatMaps = newSplatMaps;
     }
 
     private void ExpandSplatMaps(int delta)
